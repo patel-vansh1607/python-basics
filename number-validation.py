841d4aac730753ed349a90500dae419138b56593
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-07 17:31:10 +0300

    Add number validation script to compare first and last elements of two lists

---
+++ number-validation.py
@@ -0,0 +1,10 @@
+num1 = [10,20,30,40]
+num2 = [50,60,70,60]
+
+first = num1[0]
+second = num2[-1]
+
+if first == second:
+    print("The first and last numbers are equal.")
+else:
+    print("The first and last numbers are not equal.")
