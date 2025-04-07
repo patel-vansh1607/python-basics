be329976640d7be0e2f9c71a6c0a8d9809832e4b
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-07 17:05:10 +0300

    Add grade evaluation script

---
+++ grades.py
@@ -0,0 +1,14 @@
+print("---------------------------")
+
+grade = int(input("Enter your Grade = "))
+
+if grade >= 90:
+    print("Grade: A 🎉")
+elif grade >=80:
+    print("Grade: B 👍")
+elif grade >=70:
+    print("Grade: C 😐")
+elif grade >=60:
+    print("Grade: D 😞")
+else:
+    print("Grade: F 😔")
