2723d4cfc0a1254531392cc95a05fe260457cf1d
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-07 17:05:06 +0300

    Add interactive animal preference prompt

---
+++ exercise.py
@@ -0,0 +1,13 @@
+print("Enter your favourite animal")
+print("---------------------------")
+
+animal = str(input("Enter your favourite animal: "))
+
+if animal == "dog":
+    print("Dogs are loyal and awesome.")
+elif animal == "cat":
+    print("Cats are cute and love naps")
+elif animal == "lion":
+    print("Lions are the kings of the jungle.")
+else:
+    print("That's a cool animal")
