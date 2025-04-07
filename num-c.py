849847ceabc4fd6a9d8cf4dbe91a7fd1d20aedfa
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-07 17:47:05 +0300

    Add number classification script to determine if input is negative, neutral, or positive

---
+++ num-c.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number: "))
+
+if num <0:
+    print("Negative number")
+elif num == 0:
+    print("Neutral Number")
+elif num > 0 :
+    print("Positive Number")
+else:
+    print("Invalid Number")
