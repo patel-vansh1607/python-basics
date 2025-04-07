59089ad16a237a1d0e585e4eef8f6d7cee63778a
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-07 17:23:45 +0300

    Add else clause to handle multiplication result less than 1000

--- number.py
+++ number.py
@@ -6,3 +6,5 @@ num2 = int(input("Enter second number: "))
 if num1 * num2 > 1000:
     sum = num1 + num2
     print(sum)
+else:
+    ("The Multiplication is less than 1000, try another combination of numbers")
