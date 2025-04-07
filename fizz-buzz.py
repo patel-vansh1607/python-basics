b98da9a11884b7862cb994d440705465ded823e5
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-07 17:37:26 +0300

    Add FizzBuzz program to print Fizz, Buzz, or FizzBuzz based on user input

---
+++ fizz-buzz.py
@@ -0,0 +1,12 @@
+for i in range(1, 100):
+
+num = int(input("Enter a number: "))
+
+if num % 3 = 0 :
+    print("Fizz")
+elif num % 5 = 0 :
+    print("Buzz")
+elif num % 3 = 0 and num % 5 = 0 :  
+    print("FizzBuzz")
+else:
+    print(num)
