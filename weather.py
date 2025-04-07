2035fec65b454b0eabc72f048655cdfbc8cd518d
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-07 17:11:46 +0300

    Add weather advice program

---
+++ weather.py
@@ -0,0 +1,12 @@
+print ("----------")
+
+weather = str(input("Enter the weather, to get advice on what to wear: "))
+
+if weather == "sunny":
+    print("Wear SunGlasses 😎")
+elif weather == "rainy":
+    print("Take an umbrella ☔")
+elif weather == "cold":
+    print("Wear a jacket 🧥")
+else:
+    print("Stay safe out there!")
