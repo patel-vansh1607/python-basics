c2dc949bad37bfa3586e49c8c8a494d44e507103
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-10 16:28:58 +0300

    adding notes

--- main.py
+++ main.py
@@ -106,5 +106,13 @@ print(result)
 = Tuples are like lists but immutable, perfect for mixed data.
 = Dictionaries gives you fast access to data using keys.
 = Using the right data structures makes your code faster.
-= Looking up at an item in a dictionary is much faster thab a searching list
+= Looking up at an item in a dictionary is much faster thab a searching list.
+= A set is great when you want to remove duplicates.
+= Well chosen data structures make code easier to understand
+    e.g 
+    instead of:-
+            names = ["Tilak", "Ansh", "Esha"]
+            ages = ["18", "18", "20"]
+        We can do this:
+            students = {"Tilak" : 18,"Ansh" : 18,"Esha" : 20}
 """
