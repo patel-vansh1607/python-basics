fbb22133392b6d5884ddf29111a5b27d22a0a2b9
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-10 17:34:42 +0300

    Add file handling examples and explanations to fileHandling.py

--- fileHandling.py
+++ fileHandling.py
@@ -3,3 +3,34 @@
 """
 = File handling is essential for reading from and writing to files, as well as manipulating strings effectively.
 """
+
+# FILE HANDLING
+"""
+What is file handling?
+= Working with external data (txt,csv,json)
+"""
+
+# OPENING AND READING FILES
+# OPENING A FILE
+file = open("example.txt")
+content = file.read()
+print(content)
+file.close()
+# USING WITH
+with open("example.txt") as file:
+    content = file.read()
+    print(content)
+# READING WITH
+with open("example.txt", "r") as file:
+    content = file.read()
+    print(content)
+# READING LINE BY LINE
+with open("example.txt", "r") as file:
+    for line in file:
+        print(line.strip())  
+# WRITING TO FILES
+with open("example.txt", "w") as file:
+    file.write("Hello Rift Pro Software Engineering")
+# APPEND TO A FILE
+with open("output.txt", "a") as file:
+    file.write("This is a new line Rift Pro Software Engineering\n")
