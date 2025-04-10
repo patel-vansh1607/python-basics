032b4ae11769b3607e1d772502dcf9c1e8531a0b
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-10 16:23:11 +0300

    adding more notes

--- main.py
+++ main.py
@@ -95,3 +95,16 @@ def add_sum(a,b):
     return a + b
 result = add_sum(10, 20)
 print(result)
+
+
+# WHY DATA STRUCTURES MATTER
+
+"""
+= They organize, store and manage data in ways that make your program more efficient, readable, and powerful.
+= Diffrent problems need diffrent way to store and retrieve data.
+= Lists are a great way for ordered collection you change .
+= Tuples are like lists but immutable, perfect for mixed data.
+= Dictionaries gives you fast access to data using keys.
+= Using the right data structures makes your code faster.
+= Looking up at an item in a dictionary is much faster thab a searching list
+"""
