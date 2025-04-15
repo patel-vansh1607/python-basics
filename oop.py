a01ce5dca5d2f85765d2cbdc4f373cb9d1cd488c
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-15 16:38:40 +0300

    opp

---
+++ oop.py
@@ -0,0 +1,22 @@
+# OBJECT ORIENTED PROGRAMMING IN PYTHON
+
+"""
+= OOP is a programming pradigm that organzises code around objects rather than functions or procedures
+= Objects are instances of classes, wich act as blue prints defining the properties (data) and behaviours
+(fucntions/ methods) of those objects.
+= OOP models reaa world entities, making code mofre intuitive and modular.
+"""
+
+# CONECEPTS OF OOP
+"""
+= Classes & Objects
+    - A class is a template and an object is an instance of that class
+= Encapsulation 
+    - Building data (attributes) and methods (functions) within a class, controlling access to protect data integrity
+= Inheritance
+    - A class can inherit attributes and methods from another class promotting code reuse
+= Polymorpshism
+    - Objects of diffrent classes can be treated as objects of a common baseclass, allowing flexibilty
+= Abstraction
+    - Hiding complex inplementation details and exposing only necessary features to the user
+"""
